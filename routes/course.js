8e41aad69b5dc900aa4fb1800857b47ab15c36bc
Uday-6145 <udaypratap6145@gmail.com>
2026-09-14 17:55:12 +0530

    completed course route

--- routes/course.js
+++ routes/course.js
@@ -1,16 +1,32 @@
 const {Router} = require('express')
-
+const userMiddleware = require('../middleware/user')
+const {PurchaseModel, CourseModel} = require('../db.js')
 const courseRouter = Router();
 
 
-courseRouter.post('/purchase', (req, res)=> {
+courseRouter.post('/purchase', userMiddleware, async(req, res)=> {
+    const userId = req.userId;
 
+    const courseId = req.body;
+
+    await  PurchaseModel.create({
+        userId,
+        courseId
+    })
+
+    res.json({
+        message: "You have successfully purchased the course!!!!!"
+    }) 
 })
 
-courseRouter.get('/preview', (req, res)=> {
-    res.send('courses endpoint')
+// courses toh dikhne chiye user ko, So this not need to be authenticated
+courseRouter.get('/preview', async(req, res)=> {
+    const courses = await CourseModel.find({})
+
+
+res.json({courses})
 })
 
-
+ 
 module.exports =  courseRouter
 
